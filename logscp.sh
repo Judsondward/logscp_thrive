a2c037b9cb24e65f423e1848f935dd1ae72d3253
root <root@GRS0006.GranburyRS.local>
2020-05-27 18:00:36 -0500

    Adding script and updating readme

---
+++ logscp.sh
@@ -0,0 +1,10 @@
+if [ -z $1 ]
+then
+        i=10.10.10.99
+else
+        i=$1
+fi
+
+j=$(ssh firefly@$i -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null 'if [[ -f /var/log/granbury/thrive-server.log ]] ; then echo "/var/log/granbury/thrive-server.log" ; elif [[ -f /opt/thrive/wildfly-10.1.0.Final/standalone/log/server.log ]]; then echo "/opt/thrive/wildfly-10.1.0.Final/standalone/log/server.log" ; elif [[ -f /home/firefly/jboss-as-7.1.1.Final/standlong/log/server.log ]];  then echo "/home/firefly/jboss-as-7.1.1.Final/standlong/log/server.log" ; else echo "Issue locating server.log" ; fi ;')
+
+scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null firefly@$i:$j .
