0557ce2d3b89bad605eeec9c8de986ad00721392
root <root@GRS0006.GranburyRS.local>
2020-05-27 19:55:36 -0500

    Fixed fail case

--- logscp.sh
+++ logscp.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 if [ -z $1 ]
 then
         i=10.10.10.99
@@ -5,6 +6,11 @@ else
         i=$1
 fi
 
-j=$(ssh firefly@$i -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null 'if [[ -f /var/log/granbury/thrive-server.log ]] ; then echo "/var/log/granbury/thrive-server.log" ; elif [[ -f /opt/thrive/wildfly-10.1.0.Final/standalone/log/server.log ]]; then echo "/opt/thrive/wildfly-10.1.0.Final/standalone/log/server.log" ; elif [[ -f /home/firefly/jboss-as-7.1.1.Final/standlong/log/server.log ]];  then echo "/home/firefly/jboss-as-7.1.1.Final/standlong/log/server.log" ; else echo "Issue locating server.log" ; fi ;')
+j=$(ssh firefly@$i -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null 'if [[ -f /var/log/granbury/thrive-server.log ]] ; then echo "/var/log/granbury/thrive-server.log" ; elif [[ -f /opt/thrive/wildfly-10.1.0.Final/standalone/log/server.log ]]; then echo "/opt/thrive/wildfly-10.1.0.Final/standalone/log/server.log" ; elif [[ -f /home/firefly/jboss-as-7.1.1.Final/standlong/log/server.log ]];  then echo "/home/firefly/jboss-as-7.1.1.Final/standlong/log/server.log" ; else echo "" ; fi ;')
 
-scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null firefly@$i:$j .
+if [ -z $j ]
+then
+	echo "Error Finding Log File, check version"
+else
+	scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null firefly@$i:$j .
+fi
